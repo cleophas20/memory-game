749a00008ccb46dc76bf553c34981f7ee051af73
Cleophas <cleophasfoy@gmail.com>
2022-09-07 10:07:16 -0500

    added image folder with images

---
+++ app.js
@@ -0,0 +1,27 @@
+const cardArray = [
+  {
+    name: 'dragon',
+    img: 'images/dragon-face.png',
+  },
+  {
+    name: 'ghost',
+    img: 'images/ghost.png',
+  },
+  {
+    name: 'mage',
+    img: 'images/man-mage.png',
+  },
+  {
+    name: 'ogre',
+    img: 'images/ogre.png',
+  },
+  {
+    name: 'devil',
+    img: 'images/similing-face-with-horns.png',
+  },
+  {
+    name: 'zombie',
+    img: 'images/woman-zombie-3802939216.png',
+  },
+
+]
